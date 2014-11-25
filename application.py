de0439cbe5cf30385d3f3ade926d6bdba8789dad
Dmitry Zv <dmitryzv@hotmail.com>
2014-11-25 13:07:09 -0800

    Support to get back end address from env variable

--- application.py
+++ application.py
@@ -8,9 +8,11 @@ from flask import request, Response
 # Create the Flask app
 app = flask.Flask(__name__)
 
+back_end = os.environ['FIBONACCI_BE_ADDRESS']
+
 @app.route('/')
 def welcome():
-    return flask.render_template('index.html')
+    return flask.render_template('index.html', back_end = back_end)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int("80"))
